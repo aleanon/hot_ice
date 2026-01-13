713b0869c58ad73060e30b45d58956f47d6722e8
= <forsvantn@protonmail.com>
2026-01-13 08:35:10 +0100

    Added hot_state example

---
+++ examples/hot_state/src/hot_state/src/main.rs
@@ -0,0 +1,36 @@
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use ui::State;
+
+fn main() {
+    init_tracing();
+
+    let reloader_settings = hot_ice::ReloaderSettings {
+        compile_in_reloader: false,
+        ..Default::default()
+    };
+
+    hot_ice::application(State::boot, State::update, State::view)
+        .subscription(State::subscription)
+        .theme(State::theme)
+        .style(State::style)
+        .scale_factor(State::scale_factor)
+        .title(State::title)
+        .reloader_settings(reloader_settings)
+        .run()
+        .unwrap();
+}
+
+pub fn init_tracing() {
+    let fmt_layer = fmt::layer().compact();
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default())
+        .init();
+}
