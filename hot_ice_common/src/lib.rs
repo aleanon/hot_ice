51a69b09544b286c7f551c15b27f7b8b5fa58697
= <forsvantn@protonmail.com>
2026-01-15 13:59:25 +0100

    Added function name for the font syncing function

--- hot_ice_common/src/lib.rs
+++ hot_ice_common/src/lib.rs
@@ -2,3 +2,4 @@
 pub const SERIALIZE_STATE_FUNCTION_NAME: &str = "serialize_state_slkdfjaf3lasjfdsa";
 pub const DESERIALIZE_STATE_FUNCTION_NAME: &str = "deserialize_state_sldafjal3lkfjasldf";
 pub const FREE_SERIALIZED_DATA_FUNCTION_NAME: &str = "free_serialized_data_lsadkjfa3alfjda";
+pub const LOAD_FONT_FUNCTION_NAME: &str = "load_font_into_system_lskdjfa3lkfjasdf";
