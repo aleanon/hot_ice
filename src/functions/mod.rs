0f69e920ee43f35b856a39f9ca084cc2a19f09dd
= <forsvantn@protonmail.com>
2026-02-02 22:46:56 +0100

    Move hot function modules and boot into src/functions/

---
+++ src/functions/mod.rs
@@ -0,0 +1,8 @@
+pub(crate) mod boot;
+pub(crate) mod hot_scale_factor;
+pub(crate) mod hot_style;
+pub(crate) mod hot_subscription;
+pub(crate) mod hot_theme;
+pub(crate) mod hot_title;
+pub(crate) mod hot_update;
+pub(crate) mod hot_view;
